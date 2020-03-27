497bc12e470c927e7b8579e386222c3f3d8f120e
sbrylka <sbrylka96@gmail.com>
2020-03-27 18:25:37 +0100

    Started Chapter 4 exercises

---
+++ Chapter 4 - working with lists/magicians.py
@@ -0,0 +1,6 @@
+magicians = ['alicja', 'dawid', 'karolina']
+for magician in magicians:
+	print(magician.title() + ", to była doskonała sztuczka!")
+	print("Nie mogę się doczekać Twojej kolejnej sztuczki, " + magician.title() + ".\n")
+
+print("Dziękuję wszystkim, to był wspaniały występ!")
