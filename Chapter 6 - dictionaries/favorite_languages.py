2fe10b1ff8d026a3e0ee9563308e10f99fdd63d7
sbrylka <sbrylka96@gmail.com>
2020-04-06 18:18:38 +0200

    Added Chapter 6: favorite_languages.py and users.py files

---
+++ Chapter 6 - dictionaries/favorite_languages.py
@@ -0,0 +1,43 @@
+favorite_languages = {
+	'janek': 'python',
+	'sara': 'c',
+	'edward': 'ruby',
+	'paweł': 'python',
+	}
+
+print("Ulubiony język programowania Sary to " + 
+	favorite_languages['sara'].title() + ".")
+
+for name, language in favorite_languages.items():
+	print("Ulubiony język programowania użytkownika " + name.title() +
+		" to " + language.title() + ".")
+
+for name in favorite_languages.keys():
+	print(name.title())
+	
+for name in favorite_languages:
+	print(name.title())
+
+friends = ['paweł', 'sara']
+
+for name in favorite_languages.keys():
+	print(name.title())
+	
+	if name in friends:
+		print("Witaj, " + name.title() +
+			"! Widzę, że Twoim ulubionym językiem programowania jest " +
+			favorite_languages[name].title() + "!")
+	
+if 'elżbieta' not in favorite_languages.keys():
+	print("\nElżbieta, proszę, weż udział w naszej ankiecie!")
+	
+for name in sorted(favorite_languages.keys()):
+	print(name.title() + ", dziękujemy za udział w ankiecie!")
+	
+print("\nW ankiecie zostały wymienione następujące języki programowania:")
+for language in favorite_languages.values():
+	print(language.title())
+	
+print("\nW ankiecie zostały wymienione następujące języki programowania:")
+for language in set(favorite_languages.values()):
+	print(language.title())
---
+++ Chapter 6 - dictionaries/users.py
@@ -0,0 +1,9 @@
+user_0 = {
+	'usermane': 'jkowalski',
+	'first': 'jan',
+	'last': 'kowalski',
+	}
+
+for key, value in user_0.items():
+	print("\nKlucz: " + key)
+	print("Wartość: " + value)
