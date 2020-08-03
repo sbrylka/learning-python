fd4e3953f0f03706c737d66c77363e312fc7f26c
sbrylka <sbrylka96@gmail.com>
2020-08-03 22:11:57 +0200

    Completed another exercise

---
+++ Chapter 6 - dictionaries/Do it yourself/osoby.py
@@ -0,0 +1,25 @@
+osoba1 = {
+	'first_name': 'Yarek',
+	'last_name': 'Curiechenko',
+	'age': 18,
+	'city': 'Lwów',
+	}
+
+osoba2 = {
+	'first_name': 'Ania',
+	'last_name': 'Kowalska',
+	'age': 19,
+	'city': 'Częstochowa',
+	}
+
+osoba3 = {
+	'first_name': 'Marek',
+	'last_name': 'Nowak',
+	'age': 22,
+	'city': 'Racibóż',
+	}
+
+osoby = [osoba1, osoba2, osoba3]
+
+for osoba in osoby:
+	print(osoba)
