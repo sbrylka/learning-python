439a5b67fc9533e119340739e9af45c824a8ef25
sbrylka <sbrylka96@gmail.com>
2020-08-04 12:05:41 +0200

    Completed another exercise

---
+++ Chapter 6 - dictionaries/Do it yourself/zwierzęta.py
@@ -0,0 +1,24 @@
+Ari = {
+	'imię': 'Ari',
+	'opiekun_1': 'Sebastian',
+	}
+
+Pixel = {
+	'imię': 'Pixel',
+	'opiekun_1': 'Ania',
+	'opiekun_2': 'Yarek'
+	}
+
+Plejuś = {
+	'imię': 'Playboy',
+	'opiekun_1': 'Asia',
+	'opiekun_2': 'Grzesiek'
+	}
+
+koty = [Ari, Pixel]
+psy = [Plejuś]
+
+zwierzęta = [koty, psy]
+
+for zwierzę in zwierzęta:
+	print(zwierzę)
