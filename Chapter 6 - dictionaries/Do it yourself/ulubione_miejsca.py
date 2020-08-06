fad40189d346b73f97c2afa504dddf59b4510ede
sbrylka <sbrylka96@gmail.com>
2020-08-06 22:02:16 +0200

    Completed another exercise

---
+++ Chapter 6 - dictionaries/Do it yourself/ulubione_miejsca.py
@@ -0,0 +1,8 @@
+favorite_places = {
+	'Sebastian': 'Świętochłowice',
+	'Yarek': 'Kijów',
+	'Ania': 'Częstochowa',
+	}
+
+for osoba, miejsce in favorite_places.items():
+	print(osoba + " mówił(a), że " + miejsce + " jest jej/jego ulubionym miejscem.")
