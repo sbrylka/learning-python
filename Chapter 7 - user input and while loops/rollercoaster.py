aa104d07b74088b7e409ad338d8914c2c9058621
sbrylka <sbrylka96@gmail.com>
2020-08-13 20:58:19 +0200

    Completed another exercise

---
+++ Chapter 7 - user input and while loops/rollercoaster.py
@@ -0,0 +1,7 @@
+height = input("Ile masz wzrostu (w centymetrach)? ")
+height = int(height)
+
+if height >= 90:
+	print("\nJesteś wystarczająco wysoki na przejażdżkę!")
+else:
+	print("\nBędziesz mógł się przejechać, gdy nieco urośniesz!")'
