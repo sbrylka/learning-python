3348ec7b0f3b4ce8b6876b0f92d2bbdc31a7058f
sbrylka <sbrylka96@gmail.com>
2020-08-17 19:29:56 +0200

    Completed another exercise

--- Chapter 7 - user input and while loops/parrot.py
+++ Chapter 7 - user input and while loops/parrot.py
@@ -1,2 +1,11 @@
-message = input("Powiedz mi coś o sobie, a wyświetlę to na ekranie: ")
-print(message)
+prompt = "\nPowiedz mi coś o sobie, a wyświetlę to na ekranie: "
+prompt += "\nNapisz 'koniec', aby zakończyć działanie programu. "
+
+active = True
+while active:
+	message = input(prompt)
+	
+	if message == 'koniec':
+		active = False
+	else:
+		print(message)
