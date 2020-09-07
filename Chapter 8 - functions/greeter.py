7da3b1881db616f74b1d32f0a95dc4464ca91fe7
sbrylka <sbrylka96@gmail.com>
2020-09-07 21:29:06 +0200

    Upgraded greeter

--- Chapter 8 - functions/greeter.py
+++ Chapter 8 - functions/greeter.py
@@ -1,6 +1,20 @@
-def greet_user(username):
-	"""Wyświetla proste powitanie."""
-	print("Witaj, " + username.title() + "!")
+def get_formatted_name(first_name, last_name):
+	"""Zwraca sfromatowane pełne imię i nazwisko"""
+	full_name = first_name + ' ' + last_name
+	return full_name.title()
 
-greet_user('janek')
-greet_user('sebastian')
+while True:
+	print("\nProszę podać imię i nazwisko:")
+	print("(wpisz 'q', aby zakończyć pracę w dowolnym momencie")
+	
+	f_name = input("Imię: ")
+	if f_name == 'q':
+		break
+	
+	l_name = input("Nazwisko: ")
+	if l_name == 'q':
+		break
+	
+	formatted_name = get_formatted_name(f_name, l_name)
+	print("\nWitaj, " + formatted_name + "!")
+	
