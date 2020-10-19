d9bd0065472f9d7aa221bf45b23d2080f922a3ee
sbrylka <sbrylka96@gmail.com>
2020-10-19 13:26:37 +0200

    Updated an exercise

--- Chapter 9 - classes/car.py
+++ Chapter 9 - classes/car.py
@@ -6,11 +6,45 @@ class Car():
 		self.make = make
 		self.model = model
 		self.year = year
+		self.odometer_reading = 0
 		
 	def get_descriptive_name(self):
 		"""Zwrot elegancko sformatowanego opisu samochodu."""
 		long_name = str(self.year) + ' ' + self.make + ' ' + self.model
 		return long_name.title()
 		
+	def read_odometer(self):
+		"""Wyświetla informację o przebiegu samochodu."""
+		print("Ten samochód ma przejechane " + str(self.odometer_reading)
+		+ " km.")
+		
+	def update_odometer(self, mileage):
+		"""Przypisanie wartości przebiegu samochodu.
+		Zmiana zostanie odrzucona przy próbie cofnięcia licznika."""
+		if mileage >= self.odometer_reading:
+			self.odometer_reading = mileage
+		else:
+			print("Nie można cofnąć licznika przebiegu samochodu!")
+			
+	def increment_odometer(self, kilometers):
+		"""Inkrementacja wartości licznika samochodu o podaną wartość"""
+		self.odometer_reading += kilometers
+		
 my_new_car = Car('audi', 'a4', 2016)
 print(my_new_car.get_descriptive_name())
+my_new_car.read_odometer()
+
+#my_new_car.odometer_reading = 23
+#my_new_car.read_odometer()
+
+my_new_car.update_odometer(23)
+my_new_car.read_odometer()
+
+my_used_car = Car('subaru', 'outback', 2013)
+print(my_used_car.get_descriptive_name())
+
+my_used_car.update_odometer(23500)
+my_used_car.read_odometer()
+
+my_used_car.increment_odometer(100)
+my_used_car.read_odometer()
