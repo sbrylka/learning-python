e36e41f3837b991489cff26726082c9f5ab53592
sbrylka <sbrylka96@gmail.com>
2020-10-29 18:55:09 +0100

    Added new exercise

--- Chapter 9 - classes/car.py
+++ Chapter 9 - classes/car.py
@@ -30,21 +30,21 @@ class Car():
 		"""Inkrementacja wartości licznika samochodu o podaną wartość"""
 		self.odometer_reading += kilometers
 		
-my_new_car = Car('audi', 'a4', 2016)
-print(my_new_car.get_descriptive_name())
-my_new_car.read_odometer()
+#my_new_car = Car('audi', 'a4', 2016)
+#print(my_new_car.get_descriptive_name())
+#my_new_car.read_odometer()
 
 #my_new_car.odometer_reading = 23
 #my_new_car.read_odometer()
 
-my_new_car.update_odometer(23)
-my_new_car.read_odometer()
+#my_new_car.update_odometer(23)
+#my_new_car.read_odometer()
 
-my_used_car = Car('subaru', 'outback', 2013)
-print(my_used_car.get_descriptive_name())
+#my_used_car = Car('subaru', 'outback', 2013)
+#print(my_used_car.get_descriptive_name())
 
-my_used_car.update_odometer(23500)
-my_used_car.read_odometer()
+#my_used_car.update_odometer(23500)
+#my_used_car.read_odometer()
 
-my_used_car.increment_odometer(100)
-my_used_car.read_odometer()
+#my_used_car.increment_odometer(100)
+#my_used_car.read_odometer()
